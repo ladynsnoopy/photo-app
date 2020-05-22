e92bbc63341b1b9a58aba9203a7c3ef4308528c2
ladynsnoopy <49613300+ladynsnoopy@users.noreply.github.com>
2020-05-22 18:14:17 +0800

    new commit

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 const name = 'Josh Perez';
 // const element = <h1>Hello, {name}</h1>;
 
-ReactDOM.render(<h3>Hi world!</h3>,document.getElementById('app'));
+ReactDOM.render(<h3>Hi world! Myat here</h3>,document.getElementById('app'));
